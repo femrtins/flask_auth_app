cd87c5b98336ed430cefac0ce5d7a5fdf4e040de
Fernanda Martins <102699073+femrtins@users.noreply.github.com>
2024-06-17 16:20:21 -0300

    Add files via upload

--- project/auth.py
+++ project/auth.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, send_file,jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User
-from flask_login import login_user, logout_user, login_required
+from .models import User, Post, Follow
+from flask_login import login_user, logout_user, login_required, current_user
 from . import db
+from datetime import datetime
+from io import BytesIO
 
 auth = Blueprint('auth', __name__)
 
@@ -12,6 +14,7 @@ def login():
 
 @auth.route('/login', methods=['POST'])
 def login_post():
+    
     
     email = request.form['email']
     senha = request.form['senha']
@@ -30,7 +33,7 @@ def login_post():
 
     # Se os anteriores passarem manda o usuário para o seu perfil
     login_user(user, remember=remember)
-    return redirect(url_for('main.profile'))
+    return redirect(url_for('main.profile', username=user.username))
 
 @auth.route('/signup')
 def signup():
@@ -41,7 +44,6 @@ def signup():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
-
 
 '''
 Cadastrar usuário no banco de dados
@@ -57,16 +59,21 @@ e ele perca todos os dados se já existir um email igual no bd.
 @auth.route('/signup', methods=['POST'])
 def signup_post():
     nome = request.form['nome']
+    username = request.form['user_name']
     email = request.form['email']
     senha = request.form['senha']
     senha_confirmacao = request.form['senha_comparacao']
 
     user = User.query.filter_by(email=email).first()
+    user_n = User.query.filter_by(username=username).first()
 
     # Se o usuário não é encontrado, ele é redirecionado para a página de cadastro e recebe
     # a mensagem que o endereço de e-mail já exite
     if user: 
         flash('Esse e-mail já existe')
+        return redirect(url_for('auth.signup'))
+    if user_n:
+        flash('Esse nome de usuário já existe')
         return redirect(url_for('auth.signup'))
     
     # Se as senhas não são iguais o usuário é redirecionado para a página de cadastro e recebe
@@ -76,10 +83,113 @@ def signup_post():
         return redirect(url_for('auth.signup'))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=nome, password=generate_password_hash(senha, method='pbkdf2'))
+    new_user = User(email=email, name=nome, username=username, password=generate_password_hash(senha, method='pbkdf2'))
 
     # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
 
     return redirect(url_for('auth.login'))
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@auth.route('/edit_profile', methods=['GET','POST'])
+@login_required
+def edit_profile():
+    if request.method == 'POST':
+
+        bio = request.form.get('bio')
+        name = request.form.get('nome')
+        username = request.form.get('username')
+        photo = request.files.get('photo')
+
+        print(photo)
+        # Verifica se o novo email não pertence a outro usuário
+        if username != current_user.username and User.query.filter_by(username=username).first():
+            flash('Esse nome de usuário já está em uso.')
+            return redirect(url_for('auth.profile'))
+
+        # Atualiza as informações do usuário
+        if bio:
+            current_user.biography = bio
+        if name:   
+            current_user.name = name           
+        if username:      
+            current_user.username = username
+        if photo and allowed_file(photo.filename):
+            photo_bytes = photo.read()
+            current_user.image = photo_bytes
+        
+        # Salva as alterações no banco de dados
+        try:
+            db.session.commit()
+            flash('Informações do perfil atualizadas com sucesso!')
+        except Exception as e:
+            db.session.rollback()
+            flash(f'Ocorreu um erro ao atualizar o perfil: {str(e)}')
+    return redirect(url_for('main.profile', username=current_user.username))
+
+@auth.route('/post', methods=['GET','POST'])
+@login_required
+def post():
+    if request.method == 'POST':
+        post_content = request.form.get('post')
+
+        if post_content:
+            new_post = Post(post=post_content, user_id=current_user.id, username=current_user.username) # Associar post ao usuário atual
+            db.session.add(new_post)
+            try:
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                flash(f'Ocorreu um erro ao criar o post: {str(e)}')
+                
+    return redirect(url_for('main.profile', username=current_user.username))
+
+@auth.route('/del_post/<int:post_id>', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        flash('Você não tem permissão para excluir este post.')
+        return redirect(url_for('main.profile', username=current_user.username))  
+    try:
+        db.session.delete(post)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        flash(f'Ocorreu um erro ao excluir o post: {str(e)}')
+    
+    return redirect(url_for('main.profile', username=current_user.username))
+
+@auth.route('/user/image/<int:user_id>')
+def get_user_image(user_id):
+    user = User.query.get_or_404(user_id)
+    if not user.image:
+        # Se o usuário não tiver uma imagem, retorne uma imagem padrão ou uma mensagem de erro
+        return send_file('static/img/foto.png')
+    return send_file(BytesIO(user.image), mimetype='image/jpeg')
+
+@auth.route('/follow/<int:user_id>', methods=['POST'])
+@login_required
+def follow(user_id):
+    followed_user = User.query.get_or_404(user_id)    
+    if Follow.query.filter_by(follower_id=current_user.id, followed_id=followed_user.id).first():
+        return '', 204
+    follow = Follow(follower_id=current_user.id, followed_id=followed_user.id)
+    db.session.add(follow)
+    db.session.commit()
+    return redirect(url_for('main.profile', username=followed_user.username))
+
+@auth.route('/unfollow/<int:user_id>', methods=['POST'])
+@login_required
+def unfollow(user_id):
+    followed_user = User.query.get_or_404(user_id)
+    follow = Follow.query.filter_by(follower_id=current_user.id, followed_id=followed_user.id).first()
+    if follow:
+        db.session.delete(follow)
+        db.session.commit()
+    return '', 204
--- project/main.py
+++ project/main.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from . import db
+from .models import User, Post, Rating
+import requests
+
+POSTS_PER_PAGE = 20
 
 main = Blueprint('main', __name__)
 
@@ -8,7 +12,125 @@ main = Blueprint('main', __name__)
 def index():
     return render_template("index.html")
 
-@main.route('/profile')
+
+@main.route('/profile/<username>')
+# @login_required
+def profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(user_id=user.id).order_by(Post.timestamp.desc()).all()   
+    ratings = Rating.query.filter_by(user_id=user.id)
+    
+
+    for rating in ratings:
+        book_details = search_in_google_books(rating.book_id)
+        rating.book_title = book_details[0]['title']
+        rating.book_thumb= book_details[0]['thumbnail']
+
+    return render_template('profile.html', user=user, posts=posts, rating=ratings)
+
+@main.route('/stream')
+def stream():
+    page = request.args.get('page', 1, type=int)  # Pega o número da página da consulta de URL, padrão para 1
+
+    filter_type = request.args.get('filter', 'all')  # Pega o parâmetro de consulta 'filter'
+
+    if filter_type == 'following':
+        following_ids = current_user.get_following_ids()
+        posts = Post.query.filter(Post.user_id.in_(following_ids)).order_by(Post.timestamp.desc()).paginate(page=page, per_page=10)
+    else:
+        posts = Post.query.order_by(Post.timestamp.desc()).paginate(page=page, per_page=POSTS_PER_PAGE )
+
+    return render_template('stream.html', posts=posts, filter_type=filter_type)
+
+@main.route('/search')
+def search_posts():
+    query = request.args.get('query')
+    posts = Post.query.filter(Post.post.ilike(f'%{query}%')).all()
+    return render_template('stream.html', posts=posts)
+
+    
+def search_in_google_books(query):
+    url = f"https://www.googleapis.com/books/v1/volumes?q={query}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        books = response.json().get('items', [])
+        results = []
+        for item in books:
+            book_info = item.get('volumeInfo', {})
+            title = book_info.get('title')
+            authors = ', '.join(book_info.get('authors', []))
+            thumbnail = book_info.get('imageLinks', {}).get('thumbnail', '')
+            book_id = item.get('id')
+            results.append({'title': title, 'authors': authors, 'thumbnail': thumbnail, 'book_id': book_id})
+        return results
+    return []
+
+
+@main.route('/books',  methods=['GET'])
+def search_books():
+    query = request.args.get('query')
+    if not query:
+        flash('Please enter a search term')
+        return render_template('books.html')
+    api_results = search_in_google_books(query)
+
+    return render_template('books.html', query=query, api_results=api_results)
+
+
+
+@main.route('/book/<book_id>', methods=['GET', 'POST'])
+def book_details(book_id):
+
+    book = get_book_details(book_id)
+    comments = Rating.query.filter_by(book_id=book_id).order_by(Rating.timestamp.desc()).all()
+
+    if book:
+        return render_template('book.html', book=book, comments=comments)
+    else:
+        flash('Detalhes do livro não disponíveis.')
+        return redirect(url_for('main.index'))
+
+@main.route('/add_comment', methods=['POST'])
 @login_required
-def profile():
-    return render_template('profile.html', name=current_user.name)
+def add_comment():
+    comment_text = request.form.get('text')
+    book_id = request.form.get('book_id')  
+    rate = request.form.get('fb')
+    if not comment_text:
+        flash('Comment cannot be empty')
+        return redirect(url_for('main.book_details', book_id=book_id)) 
+
+    comment = Rating(user_id=current_user.id, book_id=book_id, text=comment_text, rate=rate)
+    db.session.add(comment)
+    db.session.commit()
+    flash('Comment added successfully!')
+
+    return redirect(url_for('main.book_details', book_id=book_id)) 
+
+@main.route('/del_comment/<int:rating_id>', methods=['POST'])
+@login_required
+def delete_rating(rating_id):
+    rating = Rating.query.get_or_404(rating_id)
+    if rating.user_id != current_user.id:
+        flash('Você não tem permissão para excluir este post.')
+        return redirect(url_for('main.profile', username=current_user.username))  
+    try:
+        db.session.delete(rating)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        flash(f'Ocorreu um erro ao excluir o post: {str(e)}')
+    
+    return redirect(url_for('main.profile', username=current_user.username))
+
+
+
+def get_book_details(book_id):
+    url = f"https://www.googleapis.com/books/v1/volumes/{book_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        book = response.json()
+        if 'volumeInfo' in book:
+            return book
+    return None
+
