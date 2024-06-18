d104e5b933bff99f017e94ea5b3ae83bab80ec67
monique <98707165+nickellen@users.noreply.github.com>
2024-06-18 20:50:28 -0300

    Delete project directory

--- project/static/js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('botao-editar-perfil').addEventListener('click', function() {
-    var content = document.getElementById('editar-perfil');
-    if (content.style.display === 'none' || content.style.display === '') {
-        content.style.display = 'block';
-    } else {
-        content.style.display = 'none';
-    }
-});
-
