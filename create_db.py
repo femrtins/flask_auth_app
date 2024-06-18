b1130fd98ed52dcf19384209ecbb5902fd276ee6
monique <98707165+nickellen@users.noreply.github.com>
2024-06-18 20:50:40 -0300

    Delete create_db.py

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from project import db, create_app
-
-# Crie a aplicação Flask
-app = create_app()
-
-# Crie um contexto de aplicação e chame create_all()
-with app.app_context():
-    db.create_all()
-    print("Tabelas criadas com sucesso!")
